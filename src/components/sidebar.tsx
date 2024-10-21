fe3cea8b5c74854596ef562e164fb21a24df8185
Phat <nvtpk19hutech@gmail.com>
2024-10-21 16:53:43 +0700

    add content text navbar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -80,8 +80,8 @@ export default function Sidebar({
             <div
               className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
             >
-              <span className='font-medium'>Shadcn Admin</span>
-              <span className='text-xs'>Vite + ShadcnUI</span>
+              <span className='font-medium'>Admin</span>
+              <span className='text-xs'>Tan Phat</span>
             </div>
           </div>
 
