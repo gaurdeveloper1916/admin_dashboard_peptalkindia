b5a57255096a8f4fe9867cfbb00cbd562f0c3184
kartik <gaurdeveloper1916>
2025-05-01 22:49:43 +0530

    c

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,36 @@
+// module.exports = {
+//     apps: [
+//       {
+//         name: 'peptalk_admin_dev',
+//         script: 'npm',
+//         args: 'run dev',
+//         exp_backoff_restart_delay: 100,
+//         env: {
+//           NODE_ENV: "development",
+//         },
+//       },
+//       {
+//         name: 'peptalk_admin_prod',
+//         script: 'npm',
+//         args: 'run start:prod',
+//         exp_backoff_restart_delay: 100,
+//         env_production: {
+//           NODE_ENV: "production",
+//         },
+//       },
+//     ],
+//   };
+
 module.exports = {
-    apps: [
-      {
-        name: 'peptalk_admin_dev',
-        script: 'npm',
-        args: 'run dev',
-        exp_backoff_restart_delay: 100,
-        env: {
-          NODE_ENV: "development",
-        },
-      },
-      {
-        name: 'peptalk_admin_prod',
-        script: 'npm',
-        args: 'run start:prod',
-        exp_backoff_restart_delay: 100,
-        env_production: {
-          NODE_ENV: "production",
-        },
-      },
-    ],
-  };
+  apps: [
+    {
+      name: 'peptalk_admin_prod',
+      script: 'npx',
+      args: 'serve -s dist -l 5501',
+      cwd: '/home/ubuntu/workspace/frontend/admin_dashboard_peptalkindia',
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/admin/',
+//   plugins: [react()],
+//   server: {
+//     port: 5501, 
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
 export default defineConfig({
   base: '/admin/',
   plugins: [react()],
   server: {
-    port: 5501, 
+    port: 5501,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
+
