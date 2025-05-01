c6b4ffc1d40770b898d5f6f445dc81ed11b4e349
kartik <gaurdeveloper1916>
2025-05-01 08:05:25 +0530

    chnage routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin/',
+  
   plugins: [react()],
   server: {
     port: 5500, 
