29b5ee610174dbfb6bb89474bf25183b0f08761c
kartik <gaurdeveloper1916>
2025-05-10 01:08:02 +0530

    add chnages

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,44 +1,35 @@
 import { UserAuthForm } from './components/user-auth-form'
-import ViteLogo from '@/assets/vite.svg'
+import website from '@/assets/peptal-new.png'
 
 export default function SignIn() {
   return (
     <>
       <div className='container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0'>
-        <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
+        <div className='relative hidden h-full flex-col bg-muted p-5 text-white dark:border-r lg:flex'>
           <div className='absolute inset-0 bg-zinc-900' />
           <div className='relative z-20 flex items-center text-lg font-medium'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            Shadcn Admin
+              <img
+            src={website}
+            className='relative rounded'
+            width={100}
+            height={60}
+            alt='Vite'
+          />
+            Peptal India Admin Panel
           </div>
 
           <img
-            src={ViteLogo}
-            className='relative m-auto'
-            width={301}
+            src='https://img.freepik.com/free-vector/tablet-login-concept-illustration_114360-7863.jpg?ga=GA1.1.592990485.1745861814&semt=ais_hybrid&w=740'
+            className='relative m-auto rounded'
+            width={400}
             height={60}
             alt='Vite'
           />
 
           <div className='relative z-20 mt-auto'>
             <blockquote className='space-y-2'>
-              <p className='text-lg'>
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className='text-sm'>Sofia Davis</footer>
+              
+              <footer className='text-sm'></footer>
             </blockquote>
           </div>
         </div>
